a6268936234aec295cb4fe42d39ec52eb4b92d78
mauktikmadan <46513159+mauktikmadan@users.noreply.github.com>
2019-02-01 13:11:11 +0530

    Added

---
+++ JAVA WORK/COLLECTIONS/SRC/LinkedListDemo1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class LinkedListDemo1 {
+	public static void main(String args[])
+	{
+		LinkedList<String> ll=new LinkedList<String>();
+		System.out.println("Initial size of ll:"+ll.size());
+		ll.add("C");
+		ll.add("A");
+		ll.add("F");
+		ll.add("D");
+		ll.add("E");
+		ll.add(1,"A2");
+		ll.addLast("z");
+		ll.addFirst("A");
+
+		System.out.println("Initial size  addition ll:"+ll);
+
+		ll.remove("F");
+		ll.remove(2);
+
+		System.out.println("after deletion ll:"+ll);
+		ll.removeFirst();
+		ll.removeLast();
+		System.out.println("after deletion of first and last ll:"+ll);
+		String val=ll.get(2);
+		ll.set(2,"5");
+		System.out.println("changed list:"+ll);
+	}
+}
